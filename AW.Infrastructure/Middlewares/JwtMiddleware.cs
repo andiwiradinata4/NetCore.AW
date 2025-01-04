6bbc96f2d743ca4db593d70607cf6b3cabaf1731
Andi Wiradinata <71416495+andiwiradinata4@users.noreply.github.com>
2025-01-04 08:01:54 +0700

    recommit

--- AW.Infrastructure/Middlewares/JwtMiddleware.cs
+++ AW.Infrastructure/Middlewares/JwtMiddleware.cs
@@ -23,39 +23,37 @@ namespace AW.Infrastructure.Middlewares
 
         public async Task Invoke(HttpContext context)
         {
-            var authToken = context.Request.Cookies["AuthToken"];
-            if (!string.IsNullOrEmpty(authToken))
-            {
-                var tokenHandler = new JwtSecurityTokenHandler();
-                try
-                {
-                    tokenHandler.ValidateToken(
-                                        authToken,
-                                        new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                                        {
-                                            ValidateIssuer = true,
-                                            ValidateAudience = true,
-                                            ValidAudience = _configuration["JWT:Audience"],
-                                            ValidateLifetime = true,
-                                            ValidateIssuerSigningKey = true,
-                                            ValidIssuer = _configuration["JWT:Issuer"],
-                                            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration[key: "JWT:SigningKey"] ?? "")),
-                                            ClockSkew = new TimeSpan(0, 1, 5),
-                                        },
-                                        out Microsoft.IdentityModel.Tokens.SecurityToken validatedToken);
+            //var authToken = context.Request.Cookies["AuthToken"];
+            //if (!string.IsNullOrEmpty(authToken))
+            //{
+            //    var tokenHandler = new JwtSecurityTokenHandler();
+            //    try
+            //    {
+            //        tokenHandler.ValidateToken(
+            //                            authToken,
+            //                            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            //                            {
+            //                                ValidateIssuer = true,
+            //                                ValidateAudience = true,
+            //                                ValidAudience = _configuration["JWT:Audience"],
+            //                                ValidateLifetime = true,
+            //                                ValidateIssuerSigningKey = true,
+            //                                ValidIssuer = _configuration["JWT:Issuer"],
+            //                                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration[key: "JWT:SigningKey"] ?? "")),
+            //                                ClockSkew = new TimeSpan(0, 1, 5),
+            //                            },
+            //                            out Microsoft.IdentityModel.Tokens.SecurityToken validatedToken);
 
-                    context.Request.Headers["Authorization"] = $"Bearer {authToken}";
-                    //context.Request.Headers.Add("Authorization", $"Bearer {authToken}");
-                    Console.WriteLine(context.Request.Headers["Authorization"]);
-                }
-                catch 
-                {
-                    // Token tidak valid, kembalikan 401
-                    context.Response.StatusCode = 401;
-                    await context.Response.WriteAsync("Unauthorized");
-                    return;
-                }                
-            }
+            //        context.Request.Headers["Authorization"] = $"Bearer {authToken}";
+            //    }
+            //    catch 
+            //    {
+            //        // Token tidak valid, kembalikan 401
+            //        context.Response.StatusCode = 401;
+            //        await context.Response.WriteAsync("Unauthorized");
+            //        return;
+            //    }                
+            //}
             await _next(context);
         }
     }
